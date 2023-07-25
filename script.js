bede7b60e030a93a040932897ea4c8894fb83b78
Yogeswarvenkatasai <112299999+464venkatsai@users.noreply.github.com>
2023-07-25 08:07:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -41,6 +41,48 @@ const ViewItems = () => {
 };
 
 // Router
+const Router = (ID_ARR)=>{
+  ID_ARR.forEach((class_)=>{
+    document.querySelectorAll(`${class_}`)[0].style.display = 'none'
+  })
+  document.querySelectorAll(`${ID_ARR[0]}`)[0].style.display = 'grid'
+}
+
+
 const change_page = (link)=>{
     window.location.href = `${link}`;
 }
+
+// Typewriter Effect
+const h1Element = document.getElementById('typing-text');
+let i = 0
+function setTypewriterAnimation() {
+    arr = ['Data Scientist','ML Engineer','Programmer','Data Analyst']
+    h1Element.textContent = `I am a ${arr[i]}`
+    if(i===4){
+        i=0
+    }
+    else{
+        i+=1
+    }
+    h1Element.classList.add('typing-animation');
+    h1Element.classList.remove('remove-typing-animation');
+}
+
+// Function to remove the animation classes
+function removeTypewriterAnimation() {
+    const h1Element = document.getElementById('typing-text');
+    h1Element.classList.remove('typing-animation');
+    h1Element.classList.add('remove-typing-animation');
+}
+
+// Initial setup with animation
+setTypewriterAnimation();
+const timepass = ()=>{}
+
+// Run the animation every 3 seconds
+setInterval(function() {
+  setTimeout(timepass,1000)
+    removeTypewriterAnimation();
+    setTimeout(setTypewriterAnimation, 1000); // Delay before re-adding the animation
+}, 7500);
